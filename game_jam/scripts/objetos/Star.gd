e6bca5545f8374095b6a51064ab21ae5a5fd6b76
Wendel <wendelljuliao@hotmail.com>
2020-11-04 19:34:36 -0300

    correções

--- game_jam/scripts/objetos/Star.gd
+++ game_jam/scripts/objetos/Star.gd
@@ -1,6 +1,5 @@
 extends StaticBody2D
 
-export var life = 2 
 
 signal get_a_Star()
 # Called when the node enters the scene tree for the first time.
@@ -10,11 +9,8 @@ func _ready():
 
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("Player"):
-		if life > 1:
-			$coinsAnimated.play("up")
-			emit_signal("get_a_Star")
-		life -= 1
-		if life <= 0:
-			queue_free()
+	if body.is_in_group("Player"):	
+		$coinsAnimated.play("up")
+		emit_signal("get_a_Star")
+		queue_free()
 		
